a0e077721bbabed26bc8e8b1766e35837a089bd4
myxyy <m.myxomatosis@gmail.com>
2024-11-23 18:33:11 +0900

    HeadTracker追加

--- Assets/Gimicks/DrawPosition.cs
+++ Assets/Gimicks/DrawPosition.cs
@@ -4,49 +4,52 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-public class DrawPosition : UdonSharpBehaviour
+namespace VConf2024
 {
-    [SerializeField]
-    private int _renderTextureResolution = 2048;
-    [SerializeField]
-    private RenderTexture _renderTexture;
-    [SerializeField]
-    private RenderTexture _bufferTexture;
-    [SerializeField]
-    private Transform _centerPosition;
-    [SerializeField]
-    private float _Scale = 50;
-    [SerializeField]
-    private MeshRenderer _drawMaterial;
-    [SerializeField]
-    private MeshRenderer _displayMaterial;
-    [SerializeField]
-    private Transform _testTransform;
-    [SerializeField, Tooltip("Draw time interval in seconds")]
-    private float _drawTimeInterval = 60f;
-
-
-    private void Start()
+    public class DrawPosition : UdonSharpBehaviour
     {
-        _renderTexture = new RenderTexture(_renderTextureResolution, _renderTextureResolution, 0, RenderTextureFormat.RFloat, RenderTextureReadWrite.Linear);
-        _renderTexture.filterMode = FilterMode.Point;
-        _renderTexture.Create();
-        _bufferTexture = new RenderTexture(_renderTextureResolution, _renderTextureResolution, 0, RenderTextureFormat.RFloat, RenderTextureReadWrite.Linear);
-        _bufferTexture.filterMode = FilterMode.Point;
-        _bufferTexture.Create();
-        _displayMaterial.material.SetTexture("_MainTex", _renderTexture);
-    }
+        [SerializeField]
+        private int _renderTextureResolution = 2048;
+        [SerializeField]
+        private RenderTexture _renderTexture;
+        [SerializeField]
+        private RenderTexture _bufferTexture;
+        [SerializeField]
+        private Transform _centerPosition;
+        [SerializeField]
+        private float _Scale = 50;
+        [SerializeField]
+        private MeshRenderer _drawMaterial;
+        [SerializeField]
+        private MeshRenderer _displayMaterial;
+        [SerializeField]
+        private Transform _testTransform;
+        [SerializeField, Tooltip("Draw time interval in seconds")]
+        private float _drawTimeInterval = 60f;
 
-    private void Update()
-    {
-        var relPos = _testTransform.position - _centerPosition.position;
-        var uvPos = relPos / _Scale + Vector3.one * 0.5f;
-        var uvPosVector4 = new Vector4(uvPos.x, uvPos.y, uvPos.z, Time.time);
-        _drawMaterial.material.SetVector("_UVPos", uvPosVector4);
-        VRCGraphics.Blit(_renderTexture, _bufferTexture, _drawMaterial.material);
-        VRCGraphics.Blit(_bufferTexture, _renderTexture);
 
-        _displayMaterial.material.SetFloat("_DrawInterval", _drawTimeInterval);
-        _displayMaterial.material.SetFloat("_CurrentTime", Time.time);
+        private void Start()
+        {
+            _renderTexture = new RenderTexture(_renderTextureResolution, _renderTextureResolution, 0, RenderTextureFormat.RFloat, RenderTextureReadWrite.Linear);
+            _renderTexture.filterMode = FilterMode.Point;
+            _renderTexture.Create();
+            _bufferTexture = new RenderTexture(_renderTextureResolution, _renderTextureResolution, 0, RenderTextureFormat.RFloat, RenderTextureReadWrite.Linear);
+            _bufferTexture.filterMode = FilterMode.Point;
+            _bufferTexture.Create();
+            _displayMaterial.material.SetTexture("_MainTex", _renderTexture);
+        }
+
+        private void Update()
+        {
+            var relPos = _testTransform.position - _centerPosition.position;
+            var uvPos = relPos / _Scale + Vector3.one * 0.5f;
+            var uvPosVector4 = new Vector4(uvPos.x, uvPos.y, uvPos.z, Time.time);
+            _drawMaterial.material.SetVector("_UVPos", uvPosVector4);
+            VRCGraphics.Blit(_renderTexture, _bufferTexture, _drawMaterial.material);
+            VRCGraphics.Blit(_bufferTexture, _renderTexture);
+
+            _displayMaterial.material.SetFloat("_DrawInterval", _drawTimeInterval);
+            _displayMaterial.material.SetFloat("_CurrentTime", Time.time);
+        }
     }
 }
---
+++ Assets/Gimicks/HeadTracker.cs
@@ -0,0 +1,70 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace VConf2024
+{
+    public class HeadTracker : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private string[] _targetPlayerDisplayNameList;
+        private VRCPlayerApi _targetPlayer = null;
+
+        private void Start()
+        {
+            TrySetTargetPlayer(Networking.LocalPlayer);
+        }
+
+        private void Update()
+        {
+            if (_targetPlayer != null)
+            {
+                var headPosition = _targetPlayer.GetBonePosition(HumanBodyBones.Head);
+                var headRotation = _targetPlayer.GetBoneRotation(HumanBodyBones.Head);
+                this.transform.position = headPosition;
+                this.transform.rotation = headRotation;
+            }
+        }
+
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            TrySetTargetPlayer(player);
+        }
+
+        public override void OnPlayerLeft(VRCPlayerApi player)
+        {
+            if (_targetPlayer == player)
+            {
+                _targetPlayer = null;
+            }
+            var allPlayers = new VRCPlayerApi[50];
+            VRCPlayerApi.GetPlayers(allPlayers);
+            foreach (var existPlayer in allPlayers)
+            {
+                if (existPlayer == null)
+                {
+                    continue;
+                }
+                if (TrySetTargetPlayer(existPlayer))
+                {
+                    break;
+                }
+            }
+        }
+
+        private bool TrySetTargetPlayer(VRCPlayerApi player)
+        {
+            foreach (var targetPlayerDisplayName in _targetPlayerDisplayNameList)
+            {
+                if (player.displayName == targetPlayerDisplayName)
+                {
+                    _targetPlayer = player;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
